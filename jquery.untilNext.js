ee01778e73e0f46704262098146839cdb6d6ac4d
Tobias Baaz <tobias@baaz.nu>
2010-10-22 17:47:02 +0200

    Added jquery plugin

---
+++ jquery.untilNext.js
@@ -0,0 +1,32 @@
+(function ($) {
+
+$.fn.untilNext = function (hours, minutes, callback) {
+	(function () {
+		var minutesLeft = untilNext(hours, minutes);
+		callback.call(this, minutesLeft, minutesLeft === 0);
+		window.setTimeout(arguments.callee, 500);
+	})();
+};
+
+function untilNext(hour, minute) {
+	var now = new Date(),
+		nowHour = now.getHours(),
+		nowMinute = now.getMinutes(),
+		next;
+
+	if (nowHour == hour && nowMinute == minute) {
+		return 0;
+	}
+
+	next = new Date();
+	next.setHours(hour);
+	next.setMinutes(minute);
+
+	if (next < now) {
+		next.setTime( next.getTime() + 86400000 );
+	}
+
+	return Math.floor(Math.round((next - now) / 1000) / 60);
+}
+
+})(jQuery);
